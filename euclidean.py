c40b0028efc435ddf5f79b8aec10459a708aaaf4
leo20021210 <64637030+leo20021210@users.noreply.github.com>
2021-04-22 00:17:20 +0800

    Update euclidean.py

--- euclidean.py
+++ euclidean.py
@@ -1,11 +1,14 @@
 def gcd(a,b,count=0):
     c = a%b
+    #count is the number of operations the algorith takes
     if c==0: return (b,count+1)
     return gcd(b,c,count+1)
 
-max1=0
-i1=0
-i2=0
+#calculate two integers i1 and i2 from 1-100 such that gcd(i1, i2) takes the longest time to operate
+
+max1=0 # store the maximum operation count
+i1=0 # store the first parameter of the longest gcd
+i2=0 # store the second parameter of the longest gcd
 for i in range(100):
     for j in range(100):
         if i==0 or j==0:
